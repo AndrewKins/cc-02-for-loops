60dd2933f48143bee8f1d9bb7a404a1d6d9d587a
Andrew Enoe <andrewm.enoe@gmail.com>
2021-09-29 14:41:10 +0000

    Questions 1-4 are finished, need more time to review in order to do number 5. However great, coding challenge, I learned alot :)

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 //1
 function countToTen(){
-  
+  for (let i=1; i < 11; i++){
+      console.log(i)
+  }
 }
+// countToTen()
 
 //2
-function countFromOne(){
-  
+function countFromOne(num){
+  for (let i=1; i <=num; i++){
+      console.log(i)
+  }
 }
-
+// countFromOne(50)
 //3
-function countEveryEven(){
-  
+function countEveryEven(int){
+  for (let i=1; i<=int; i++){
+      if(i % 2 === 0)
+        console.log(i)
+      }
 }
-
+// countEveryEven(10)
 //4
-function countEveryOdd(){
-  
+function countEveryOdd(odd){
+  for (let i=1; i<=odd; i++){
+      if(i % 2 !== 0)
+      console.log(i)
+    }
 }
-
+// countEveryOdd(10)
 //5 
 function countEvens(){
   
 }
 
+//Need more time to review on how to do #5.
